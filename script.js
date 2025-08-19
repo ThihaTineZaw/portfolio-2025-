1db6cd3f68ce59d571373de118b52d1ac0edd1e0
Thiha Tine Zaw <thihatinezaw.mm@gmail.com>
2025-08-19 10:50:01 +0630

    feat 2 animation & aboutMe

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-window.addEventListener('DOMContentLoaded',function(){
-    let text = document.getElementById('myname');
-let char = SplitText.create(text,{
-    type : 'chars'
-})
-let tl = gsap.timeline();
+ let imageProfile = this.document.getElementById('profile');
+ imageProfile.addEventListener('load',function(){
+    this.parentElement.classList.add('loaded-animation');
+      let text = document.getElementById('myname');
+    let char = SplitText.create(text,{type : 'chars'});
+    let tl = gsap.timeline();
 
-tl.from(char.chars,{
-    y:10,
-    stagger : 0.1,
-    opacity : 0
-})
-.from('.headerPTags',{
-    y:10,
-    opacity : 0
-})
-.from('.headerSocials',{
-     y:10,
-    opacity : 0
-})
+    tl.from(char.chars,{y:10,stagger : 0.1,opacity : 0})
+      .from('.headerPTags',{y:10,opacity : 0})
+      .from('.headerSocials',{y:10,opacity : 0})
+ })
+    
 
 
+gsap.from('#aboutMe,#becameInterested',{
+  scrollTrigger:{
+    trigger : '#aboutMe,#becameInterested',
+  },
+  y: 50,         
+  autoAlpha:0,
+  duration: 2,
+  stagger:1
+  
 })
