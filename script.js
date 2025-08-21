918899830da6c0c00b34ff693daa97b8136ec3d5
Thiha Tine Zaw <thihatinezaw.mm@gmail.com>
2025-08-21 22:56:55 +0630

    feat:4 IconAdding

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function loadAnni() {
 };
 setTimeout(loadAnni, 3000);
 
-
 let text = document.getElementById('myname');
 let char = SplitText.create(text, { type: 'chars' });
 let tl = gsap.timeline();
@@ -38,46 +37,42 @@ gsap.from('#becameInterested', {
   stagger: 1
 
 })
-
 // Experience Section Start 
 let workExpLine = CSSRulePlugin.getRule(".workingExprienceBoxes::before");
-let learnedLine = CSSRulePlugin.getRule(".learnedBoxes::before");
-
 gsap.set('#workingExprienceBox-1',{autoAlpha:0,   x : -100});
 gsap.set('#workingExprienceBox-2',{autoAlpha:0,   x : -100});
 gsap.set('#workingExprienceBox-3',{autoAlpha:0,   x : -100});
-
-gsap.set('#learndbox-1',{autoAlpha:0,   x : -100});
-gsap.set('#learndbox-2',{autoAlpha:0,   x : -100});
-gsap.set('#learndbox-3',{autoAlpha:0,   x : -100});
-gsap.set('#learndbox-4',{autoAlpha:0,   x : -100});
-gsap.set('#learndbox-5',{autoAlpha:0,   x : -100});
-gsap.set('#learndbox-6',{autoAlpha:0,   x : -100});
-
+gsap.set('#workingExperienceSection-header',{autoAlpha:0,   x : -100});
 let ExperienceLineAndBox = gsap.timeline({
   scrollTrigger: {
     trigger: '#workingExprienceBoxes',
     start: 'top 90%'
   }
 });
-
-ExperienceLineAndBox.to(workExpLine, {cssRule: { height: "100%" },duration: 3,ease: "power2.inOut"})
+ExperienceLineAndBox.to('#workingExperienceSection-header',{x:0,autoAlpha:1})
+  .to(workExpLine, {cssRule: { height: "100%" },duration: 3,ease: "power2.inOut"})
   .to('#workingExprienceBox-1', {autoAlpha: 1,x: 0,ease: "power2.inOut"}, "<0.5")
   .to('#workingExprienceBox-2', {autoAlpha: 1,x: 0,ease: "power2.inOut"}, "<0.5")
   .to('#workingExprienceBox-3', {autoAlpha: 1,x: 0,ease: "power2.inOut"}, "<0.5");
 
-
+let learnedLine = CSSRulePlugin.getRule(".learnedBoxes::before");
+gsap.set('#learndbox-1',{autoAlpha:0,   x : -100});
+gsap.set('#learndbox-2',{autoAlpha:0,   x : -100});
+gsap.set('#learndbox-3',{autoAlpha:0,   x : -100});
+gsap.set('#learndbox-4',{autoAlpha:0,   x : -100});
+gsap.set('#learndbox-5',{autoAlpha:0,   x : -100});
+gsap.set('#learndbox-6',{autoAlpha:0,   x : -100});
+gsap.set('#learned-section-header',{autoAlpha:0,   x : -100});
 
 let learnedLineAndBox = gsap.timeline({
   scrollTrigger: {
     trigger: '#learnedBoxes',
-    markers: true,
     start: 'top 90%',
   }
 });
 
-
-learnedLineAndBox.to(learnedLine, {cssRule: { height: "100%" },duration: 3,ease: "power2.inOut"})
+learnedLineAndBox.to('#learned-section-header',{x:0,autoAlpha:1})
+.to(learnedLine, {cssRule: { height: "100%" },duration: 3,ease: "power2.inOut"})
   .to('#learndbox-1', {autoAlpha: 1,x: 0,ease: "power2.inOut"}, "<0.5")
   .to('#learndbox-2', {autoAlpha: 1,x: 0,ease: "power2.inOut"}, "<0.5")
   .to('#learndbox-3', {autoAlpha: 1,x: 0,ease: "power2.inOut"}, "<0.5")
@@ -85,8 +80,27 @@ learnedLineAndBox.to(learnedLine, {cssRule: { height: "100%" },duration: 3,ease:
   .to('#learndbox-5', {autoAlpha: 1,x: 0,ease: "power2.inOut"}, "<0.5")
   .to('#learndbox-6', {autoAlpha: 1,x: 0,ease: "power2.inOut"}, "<0.5")
   
-
-
+// icon animation start 
+gsap.fromTo('.icons',
+  { 
+    y: 100, 
+    opacity: 0, 
+    scale: 0.5 
+  },
+  { 
+    y: 0,                    
+    opacity: 1,
+    scale: 1.1,
+    duration: 1,
+    stagger: 0.2,
+    ease: "bounce.inOut",
+    scrollTrigger: {
+      trigger: '#icons-box',
+      start: "top 80%", 
+    }
+  }
+);
+// icon animation end
 
 
 
